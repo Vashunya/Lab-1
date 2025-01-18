83fc734193e5d2dd871292b9fad83a399687d928
Vashunya <vashunya15@gmail.com>
2025-01-18 18:03:16 +0500

    first commit

--- test1.py
+++
@@ -1,8 +0,0 @@
-lenght = int(input("The number of digits:"))
-feb = [1, 1]
-for i in range(1, lenght - 1):
-    feb.append(feb[i - 1] + feb[i])
-
-for i in range(0, lenght):
-    print(feb[i])
-print(sum(feb))
